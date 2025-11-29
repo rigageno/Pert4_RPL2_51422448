c0fa9768b53c7ed3f2f0936c2f595888ef6ee180
rigageno <riga.geno@yahoo.com>
2025-11-29 11:25:37 +0700

    Third Commit

---
+++ src/main/java/com/mycompany/pert4_51422448/Pert4_51422448.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pert4_51422448;
+
+/**
+ *
+ * @author DuckyDuck
+ */
+public class Pert4_51422448 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
